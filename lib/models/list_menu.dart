25648eaaf1fdcc344afe99d4aaf3c4bae6fdc5b8
rouf_majidin <roufmajidin777@gmail.com>
2023-05-02 03:56:26 +0700

    update list dummy (isi artikel)

--- lib/models/list_menu.dart
+++ lib/models/list_menu.dart
@@ -35,27 +35,35 @@ List listBerita = [
     "tanggal": "August 20",
     "waktu_pub": "09.25 AM",
     'gambar': "assets/images/contoh_3.jpg",
-    'created_by': "UPT. Dukupuntang"
+    'created_by': "UPT. Dukupuntang",
+    "isi":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue massa id neque sagittis, eu venenatis justo lobortis. Nullam dictum odio ac felis faucibus feugiat. Nullam dictum volutpat ultrices. Vestibulum tristique augue eget tellus porttitor dapibus. Maecenas mi risus, tempor eu mattis vitae, lobortis ac elit. Curabitur lobortis diam at quam posuere elementum. Vivamus ornare nisl sit amet tellus sodales facilisis. Nam sagittis dui neque, vel iaculis quam imperdiet quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer imperdiet euismod vestibulum. Nulla eget vestibulum diam. Donec vitae semper neque. Nullam ut metus eu quam lacinia consectetur a eget tortor. In viverra sem metus, a consectetur mi gravida a. Aenean at mollis nunc. Nulla facilisis consectetur viverra. Pellentesque diam ligula, cursus ac sapien in, tristique malesuada est. Quisque volutpat sem a laoreet pellentesque. Duis ultrices turpis non tortor mattis, quis rutrum est tincidunt. Sed egestas velit arcu, id dapibus lorem vulputate nec. Praesent vel nulla turpis. In ut erat auctor sem fermentum tincidunt a tincidunt massa. Sed a lacinia orci. Vestibulum imperdiet condimentum pharetra. Mauris in augue imperdiet, congue diam vitae, tincidunt ex. Duis vel mattis nulla. Quisque semper massa non purus malesuada, vel iaculis nunc semper. Aliquam dignissim consectetur purus, at malesuada nisi consectetur eu."
   },
   {
     "judulBerita": "Contoh Judul dan ini adalah judul maka judul",
     "tanggal": "August 20",
     "waktu_pub": "09.25 AM",
     'gambar': "assets/images/contoh_1.jpg",
-    'created_by': "UPT. Dukupuntang"
+    'created_by': "UPT. Dukupuntang",
+    "isi":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue massa id neque sagittis, eu venenatis justo lobortis. Nullam dictum odio ac felis faucibus feugiat. Nullam dictum volutpat ultrices. Vestibulum tristique augue eget tellus porttitor dapibus. Maecenas mi risus, tempor eu mattis vitae, lobortis ac elit. Curabitur lobortis diam at quam posuere elementum. Vivamus ornare nisl sit amet tellus sodales facilisis. Nam sagittis dui neque, vel iaculis quam imperdiet quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer imperdiet euismod vestibulum. Nulla eget vestibulum diam. Donec vitae semper neque. Nullam ut metus eu quam lacinia consectetur a eget tortor. In viverra sem metus, a consectetur mi gravida a. Aenean at mollis nunc. Nulla facilisis consectetur viverra. Pellentesque diam ligula, cursus ac sapien in, tristique malesuada est. Quisque volutpat sem a laoreet pellentesque. Duis ultrices turpis non tortor mattis, quis rutrum est tincidunt. Sed egestas velit arcu, id dapibus lorem vulputate nec. Praesent vel nulla turpis. In ut erat auctor sem fermentum tincidunt a tincidunt massa. Sed a lacinia orci. Vestibulum imperdiet condimentum pharetra. Mauris in augue imperdiet, congue diam vitae, tincidunt ex. Duis vel mattis nulla. Quisque semper massa non purus malesuada, vel iaculis nunc semper. Aliquam dignissim consectetur purus, at malesuada nisi consectetur eu."
   },
   {
     "judulBerita": "Ini adalah Judul dua pada aplikasi ini",
     "tanggal": "August 20",
     "waktu_pub": "09.25 AM",
     'gambar': "assets/images/contoh_2.jpg",
-    'created_by': "UPT. Dukupuntang"
+    'created_by': "UPT. Dukupuntang",
+    "isi":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue massa id neque sagittis, eu venenatis justo lobortis. Nullam dictum odio ac felis faucibus feugiat. Nullam dictum volutpat ultrices. Vestibulum tristique augue eget tellus porttitor dapibus. Maecenas mi risus, tempor eu mattis vitae, lobortis ac elit. Curabitur lobortis diam at quam posuere elementum. Vivamus ornare nisl sit amet tellus sodales facilisis. Nam sagittis dui neque, vel iaculis quam imperdiet quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer imperdiet euismod vestibulum. Nulla eget vestibulum diam. Donec vitae semper neque. Nullam ut metus eu quam lacinia consectetur a eget tortor. In viverra sem metus, a consectetur mi gravida a. Aenean at mollis nunc. Nulla facilisis consectetur viverra. Pellentesque diam ligula, cursus ac sapien in, tristique malesuada est. Quisque volutpat sem a laoreet pellentesque. Duis ultrices turpis non tortor mattis, quis rutrum est tincidunt. Sed egestas velit arcu, id dapibus lorem vulputate nec. Praesent vel nulla turpis. In ut erat auctor sem fermentum tincidunt a tincidunt massa. Sed a lacinia orci. Vestibulum imperdiet condimentum pharetra. Mauris in augue imperdiet, congue diam vitae, tincidunt ex. Duis vel mattis nulla. Quisque semper massa non purus malesuada, vel iaculis nunc semper. Aliquam dignissim consectetur purus, at malesuada nisi consectetur eu."
   },
   {
     "judulBerita": "Contoh Judul dan ini adalah judul maka judul",
     "tanggal": "August 20",
     "waktu_pub": "09.25 AM",
     'gambar': "assets/images/contoh_1.jpg",
-    'created_by': "UPT. Dukupuntang"
+    'created_by': "UPT. Dukupuntang",
+    "isi":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue massa id neque sagittis, eu venenatis justo lobortis. Nullam dictum odio ac felis faucibus feugiat. Nullam dictum volutpat ultrices. Vestibulum tristique augue eget tellus porttitor dapibus. Maecenas mi risus, tempor eu mattis vitae, lobortis ac elit. Curabitur lobortis diam at quam posuere elementum. Vivamus ornare nisl sit amet tellus sodales facilisis. Nam sagittis dui neque, vel iaculis quam imperdiet quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer imperdiet euismod vestibulum. Nulla eget vestibulum diam. Donec vitae semper neque. Nullam ut metus eu quam lacinia consectetur a eget tortor. In viverra sem metus, a consectetur mi gravida a. Aenean at mollis nunc. Nulla facilisis consectetur viverra. Pellentesque diam ligula, cursus ac sapien in, tristique malesuada est. Quisque volutpat sem a laoreet pellentesque. Duis ultrices turpis non tortor mattis, quis rutrum est tincidunt. Sed egestas velit arcu, id dapibus lorem vulputate nec. Praesent vel nulla turpis. In ut erat auctor sem fermentum tincidunt a tincidunt massa. Sed a lacinia orci. Vestibulum imperdiet condimentum pharetra. Mauris in augue imperdiet, congue diam vitae, tincidunt ex. Duis vel mattis nulla. Quisque semper massa non purus malesuada, vel iaculis nunc semper. Aliquam dignissim consectetur purus, at malesuada nisi consectetur eu."
   },
 ];
