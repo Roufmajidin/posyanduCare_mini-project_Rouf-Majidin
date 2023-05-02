0cb8dd432ca8ca215cbe3290934fd89cc5072376
rouf_majidin <roufmajidin777@gmail.com>
2023-05-02 17:55:02 +0700

    update UI kunjungan page

--- lib/view/detail_screen/kunjungan.dart
+++ lib/view/detail_screen/kunjungan.dart
@@ -18,51 +18,30 @@ class Kunjungan extends StatelessWidget {
 
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(200.0),
+        preferredSize: const Size.fromHeight(80.0),
         child: AppBar(
           backgroundColor: AppTheme.primaryColor,
+          centerTitle: true,
           title: Text(menu[index]['judul']),
-          flexibleSpace: Padding(
-            padding: const EdgeInsets.only(top: 130.0, left: 20, right: 20),
-            child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(40)),
-                width: mediaquery.size.width * 15,
-                height: 60,
-                alignment: Alignment.center,
-                child: TextFormField(
-                  initialValue: "Cari data",
-                  decoration: InputDecoration(
-                    // fillColor: Colors.white,
-                    prefixIcon: const Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                    ),
-                    hintText: "Search",
-
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(40),
-                        borderSide:
-                            const BorderSide(width: 2, color: Colors.white)),
-                  ),
-                )),
-          ),
+          elevation: 0.6,
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(IconlyBroken.info_square),
+            )
+          ],
         ),
       ),
-      body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          physics: NeverScrollableScrollPhysics(),
-          child: SizedBox(
-            height: mediaquery.size.height * 1,
-            child: Column(
-              children: [
-                listDataWarga(mediaquery),
-                // Spacer(),
-              ],
-            ),
-          )),
+      backgroundColor: AppTheme.primaryColor,
+      body: SizedBox(
+        height: mediaquery.size.height * 1,
+        child: Column(
+          children: [
+            listDataWarga(mediaquery),
+            // Spacer(),
+          ],
+        ),
+      ),
       floatingActionButton: ElevatedButton.icon(
           onPressed: () {
             log("add kunjungan warga");
@@ -216,125 +195,155 @@ class Kunjungan extends StatelessWidget {
     );
   }
 
-  listDataWarga(mediaQuery) {
-    return Column(
-      children: [
-        Padding(
-          padding:
-              const EdgeInsets.only(top: 30.0, left: 10, right: 10, bottom: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Data Kunjungan",
-                style: PrimaryTextStyle.judulStyle,
+  listDataWarga(mediaquery) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+            color: AppTheme.bgColor,
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                  // color: Colors.grey,
+                  borderRadius: BorderRadius.circular(12)),
+              width: mediaquery.size.width * 0.9,
+              height: 40,
+              alignment: Alignment.center,
+              child: TextFormField(
+                validator: (value) {
+                  // if (n.hasMatch(value)) {
+                  //   return "Nama harus benar";
+                  // }
+                },
+                obscureText: false,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                  labelText: 'Cari Data',
+                ),
               ),
-              Column(
-                children: const [
-                  Icon(
-                    Icons.circle,
-                    size: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 30.0, left: 10, right: 10, bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Data Kunjungan",
+                    style: PrimaryTextStyle.judulStyle,
                   ),
-                  Icon(
-                    Icons.circle,
-                    size: 5,
-                  ),
-                  Icon(
-                    Icons.circle,
-                    size: 5,
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 600,
-          width: mediaQuery.size.width * 5,
-          child: ListView.builder(
-            padding: const EdgeInsets.only(bottom: 20),
-            scrollDirection: Axis.vertical,
-            physics: ScrollPhysics(),
-            itemCount: warga.length,
-            itemBuilder: (context, index) {
-              return Card(
-                elevation: 0.2,
-                child: Container(
-                  padding: const EdgeInsets.all(12),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                          decoration: BoxDecoration(
-                              color: Colors.grey.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(12)),
-                          height: 80,
-                          width: 80,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Icon(IconlyBroken.hide),
-                              Text("no img")
-                            ],
-                          )
-                          // Image.asset(
-                          //   listBerita[index]["gambar"].toString(),
-                          //   fit: BoxFit.contain,
-                          // )
-                          ),
-                      const SizedBox(
-                        width: 20,
+                  Column(
+                    children: const [
+                      Icon(
+                        Icons.circle,
+                        size: 5,
                       ),
-                      Column(
+                      Icon(
+                        Icons.circle,
+                        size: 5,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        size: 5,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 600,
+              width: mediaquery.size.width * 5,
+              child: ListView.builder(
+                padding: const EdgeInsets.only(bottom: 20),
+                scrollDirection: Axis.vertical,
+                physics: ScrollPhysics(),
+                itemCount: warga.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 0.9,
+                    child: Container(
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            warga[index]["namaWarga"],
-                            style: PrimaryTextStyle.judulStyle,
-                          ),
+                          Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(12)),
+                              height: 80,
+                              width: 80,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Icon(IconlyBroken.hide),
+                                  Text("no img")
+                                ],
+                              )
+                              // Image.asset(
+                              //   listBerita[index]["gambar"].toString(),
+                              //   fit: BoxFit.contain,
+                              // )
+                              ),
                           const SizedBox(
-                            height: 5,
+                            width: 20,
                           ),
-                          Text(
-                            warga[index]["blok"],
-                            style: PrimaryTextStyle.subTxt,
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                warga[index]["namaWarga"],
+                                style: PrimaryTextStyle.judulStyle,
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                warga[index]["blok"],
+                                style: PrimaryTextStyle.subTxt,
+                              ),
+                            ],
                           ),
+                          Spacer(),
+                          GestureDetector(
+                            onTap: () {
+                              log("masuk Ke detail");
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => KunjunganDetail(
+                                        index: index,
+                                      )));
+                            },
+                            child: SizedBox(
+                              height: 40,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    IconlyBroken.arrow_right_2,
+                                    size: 15,
+                                    color: AppTheme.primaryColor,
+                                  ),
+                                  Text("Detail",
+                                      style: PrimaryTextStyle.judulStyle),
+                                ],
+                              ),
+                            ),
+                          )
                         ],
                       ),
-                      Spacer(),
-                      GestureDetector(
-                        onTap: () {
-                          log("masuk Ke detail");
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => KunjunganDetail(
-                                    index: index,
-                                  )));
-                        },
-                        child: SizedBox(
-                          height: 40,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                IconlyBroken.arrow_right_2,
-                                size: 15,
-                                color: AppTheme.primaryColor,
-                              ),
-                              Text("Detail",
-                                  style: PrimaryTextStyle.judulStyle),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
